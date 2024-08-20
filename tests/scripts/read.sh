af3b3022fb979d9f723eb45bf05852cdc8df85f1
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-08-20 06:42:58 +0000

    added write all data.

--- tests/scripts/read.sh
+++ tests/scripts/read.sh
@@ -10,6 +10,53 @@ DATA_DIR=${PROJECT_DIR}/build/data
 mkdir -p $DATA_DIR
 NUM_FILES=1
 NUM_OPS=$((1))
-TS=$((256*1024))
-TEST_CASE=0 #write=0 read=1 both=2
+TEST_CASE=1 #write=0 read=1 both=2
+
+TS=$((1*1024))
 mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
+sleep 5
+
+TS=$((4*1024))
+mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
+sleep 5
+
+TS=$((16*1024))
+mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
+sleep 5
+
+TS=$((64*1024))
+mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
+sleep 5
+
+TS=$((256*1024))
+mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
+sleep 5
+
+TS=$((1024*1024))
+mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
+sleep 5
+
+TS=$((4*1024*1024))
+mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
+sleep 5
+
+TS=$((16*1024*1024))
+mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
+sleep 5
+
+TS=$((64*1024*1024))
+mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
+sleep 5
+
+TS=$((256*1024*1024))
+mpirun -np 1 --use-hwthread-cpus -x LD_PRELOAD=${DATACRUMBS_SO} ${PROJECT_DIR}/build/tests/df_tracer_test ${NUM_FILES} ${NUM_OPS} ${TS} ${DATA_DIR} ${TEST_CASE}
+
