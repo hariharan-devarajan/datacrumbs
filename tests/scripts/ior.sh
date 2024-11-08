af60454a057efda3009d87845cf2a93a7d838773
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-11-08 12:30:42 -0800

    added iterations

--- tests/scripts/ior.sh
+++ tests/scripts/ior.sh
@@ -9,14 +9,13 @@ DATACRUMBS_SO=${PROJECT_DIR}/build/libdatacrumbs.so
 
 DATA_DIR=${PROJECT_DIR}/build/data
 mkdir -p $DATA_DIR
-if [ "$TEST_CASE" -eq "0" ] || [ "$TEST_CASE" -eq "2" ]; then
-  echo "Cleaning Data"
-  ls -lhs $DATA_DIR
-  rm -rf $DATA_DIR/*
-fi
+rm -rf $DATA_DIR/*
 
 for ts in 4k 16k 64k 256k 1m 4m 16m 64m; do 
-  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior -o=${DATA_DIR}/test.bat -F -m -b=1g -t=${ts} -i 10 -w -r -d 5
+  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior -o=${DATA_DIR}/test.bat -F -m -b=1g -t=${ts} -i 10 -w -k -d 5
   sleep 10
+  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior -o=${DATA_DIR}/test.bat -F -m -b=1g -t=${ts} -i 10 -r -d 5
+  sleep 10
+  rm -rf $DATA_DIR/*
 done
 
