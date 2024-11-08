c10af778961bc246b21b7be136ab6fb67317d6f2
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-11-08 12:32:19 -0800

    added iterations

--- tests/scripts/ior.sh
+++ tests/scripts/ior.sh
@@ -10,11 +10,11 @@ DATACRUMBS_SO=${PROJECT_DIR}/build/libdatacrumbs.so
 DATA_DIR=${PROJECT_DIR}/build/data
 mkdir -p $DATA_DIR
 rm -rf $DATA_DIR/*
-
 for ts in 4k 16k 64k 256k 1m 4m 16m 64m; do 
-  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior -o=${DATA_DIR}/test.bat -F -m -b=1g -t=${ts} -i 10 -w -k -d 5
+  configuration="-o=${DATA_DIR}/test.bat -F -m -b=1g -i 10 -d 5 -t=${ts}"
+  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -w -k
   sleep 10
-  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior -o=${DATA_DIR}/test.bat -F -m -b=1g -t=${ts} -i 10 -r -d 5
+  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -r
   sleep 10
   rm -rf $DATA_DIR/*
 done
