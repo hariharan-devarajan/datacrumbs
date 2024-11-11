d2b47ee1fe7917ff545b9eaf233f60fa9bb5806a
Your Name <you@example.com>
2024-11-11 00:12:54 +0000

    push latest changes

---
+++ scripts/install_spack.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo pip install -r requirements.txt
+sudo apt-get update
+sudo apt-get install -y hwloc libtool openssl libssl-dev gfortran gcc g++
+SPACK_ROOT=/opt/spack
+git clone https://github.com/spack/spack.git ${SPACK_ROOT}
+source ${SPACK_ROOT}/share/spack/setup-env.sh 
+spack external find
+spack compiler find
+spack install -j64 openmpi@5.0.5%gcc@11.4.0 hdf5@1.14.5%gcc@11.4.0
+export HDF5_DIR=$(spack location -i hdf5@1.14.5%gcc@11.4.0)
+export OPENMPI_DIR=$(spack location -i openmpi@5.0.5%gcc@11.4.0)
+echo "Set HDF5 to ${HDF5_DIR}"
+echo "Set OPENMPI to ${OPENMPI_DIR}"
+IOR_DIR=/opt/ior
+git clone git@github.com:hpc/ior.git ${IOR_DIR}
+cd $IOR_DIR
+./bootstrap
+./configure --with-hdf5=on --prefix=$PWD/install CFLAGS="-I${HDF5_DIR}/include -L${HDF5_DIR}/lib" CPPFLAGS="-I${HDF5_DIR}/include -L${HDF5_DIR}/lib"
+make install
+cd -
--- tests/scripts/ior.sh
+++ tests/scripts/ior.sh
@@ -128,34 +128,34 @@ for ts in 256k; do # 4k 16k 64k 256k 1m 4m 16m 64m
   rm -rf $DATA_DIR/*
 done
 
-for ts in 256k; do # 4k 16k 64k 256k 1m 4m 16m 64m
-  configuration="-o=${DATA_DIR}/test.bat-${ts} -F -m -b=${BLOCK} -i 10 -d 10 -t=${ts} -a HDF5"
-  echo "Running ${IOR_INSTALL_DIR}/bin/ior ${configuration} -w -k"
-  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -w -k
-  sleep 10
-  if [ "$DROP_CACHES" -eq "1" ];
-  then
-    echo "Clean Cache"
-    sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
-  fi
-  echo "Running ${IOR_INSTALL_DIR}/bin/ior ${configuration} -r"
-  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -r
-  sleep 10
-  rm -rf $DATA_DIR/*
-done
+# for ts in 256k; do # 4k 16k 64k 256k 1m 4m 16m 64m
+#   configuration="-o=${DATA_DIR}/test.bat-${ts} -F -m -b=${BLOCK} -i 10 -d 10 -t=${ts} -a HDF5"
+#   echo "Running ${IOR_INSTALL_DIR}/bin/ior ${configuration} -w -k"
+#   LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -w -k
+#   sleep 10
+#   if [ "$DROP_CACHES" -eq "1" ];
+#   then
+#     echo "Clean Cache"
+#     sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+#   fi
+#   echo "Running ${IOR_INSTALL_DIR}/bin/ior ${configuration} -r"
+#   LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -r
+#   sleep 10
+#   rm -rf $DATA_DIR/*
+# done
 
-for ts in 256k; do # 4k 16k 64k 256k 1m 4m 16m 64m
-  configuration="-o=${DATA_DIR}/test.bat-${ts} -F -m -b=${BLOCK} -i 10 -d 10 -t=${ts} -a HDF5 -c"
-  echo "Running ${IOR_INSTALL_DIR}/bin/ior ${configuration} -w -k"
-  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -w -k
-  sleep 10
-  if [ "$DROP_CACHES" -eq "1" ];
-  then
-    echo "Clean Cache"
-    sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
-  fi
-  echo "Running ${IOR_INSTALL_DIR}/bin/ior ${configuration} -r"
-  LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -r
-  sleep 10
-  rm -rf $DATA_DIR/*
-done
+# for ts in 256k; do # 4k 16k 64k 256k 1m 4m 16m 64m
+#   configuration="-o=${DATA_DIR}/test.bat-${ts} -F -m -b=${BLOCK} -i 10 -d 10 -t=${ts} -a HDF5 -c"
+#   echo "Running ${IOR_INSTALL_DIR}/bin/ior ${configuration} -w -k"
+#   LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -w -k
+#   sleep 10
+#   if [ "$DROP_CACHES" -eq "1" ];
+#   then
+#     echo "Clean Cache"
+#     sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+#   fi
+#   echo "Running ${IOR_INSTALL_DIR}/bin/ior ${configuration} -r"
+#   LD_PRELOAD=$DATACRUMBS_SO ${IOR_INSTALL_DIR}/bin/ior ${configuration} -r
+#   sleep 10
+#   rm -rf $DATA_DIR/*
+# done
